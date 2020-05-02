4b1c577e2a7a291bd2de731fbf252111bb124e42
Pandu_Pradana <pandupradana50@gmail.com>
2020-05-02 17:37:14 +0700

    Update Terbaru

--- app/src/main/java/com/application/wisatacilacap/Kuliner/KulinerBrekecek.java
+++ app/src/main/java/com/application/wisatacilacap/Kuliner/KulinerBrekecek.java
@@ -1,15 +1,94 @@
 package com.application.wisatacilacap.Kuliner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
+import com.application.wisatacilacap.Hotel.HotelAtrium;
+import com.application.wisatacilacap.Maps.MapsHotelAtrium;
+import com.application.wisatacilacap.Maps.MapsKulinerBrekecek;
 import com.application.wisatacilacap.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class KulinerBrekecek extends AppCompatActivity {
+public class KulinerBrekecek extends AppCompatActivity implements OnMapReadyCallback {
+
+    private Toolbar mToolbar;
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuliner_brekecek);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Button btn1 = (Button) findViewById(R.id.buttonmaps1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(KulinerBrekecek.this, MapsKulinerBrekecek.class);
+                startActivity(i);
+            }
+        });
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button callIntent = (Button) findViewById(R.id.telephone);
+        callIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:123"));
+                if (ActivityCompat.checkSelfPermission(KulinerBrekecek.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-7.7250609,109.0149522);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Brekecek Pathak Jahan Bu Widi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
     }
 }
